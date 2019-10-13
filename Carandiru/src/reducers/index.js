e12fb0919c03153216675b14a7a2353edb8825d0
LuizDoPc <luizotaviosoares@outlook.com>
2019-10-13 16:16:16 -0300

    add new project files

--- Carandiru/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import AppReducer from './AppReducer';
-import AuthReducer from './AuthReducer';
-
-export default combineReducers({
-  AuthReducer,
-  AppReducer
-});
